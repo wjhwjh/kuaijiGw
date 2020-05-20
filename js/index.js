c8f5273b3312035105159fdd42cfe42010555a9d
wjhwjh <1468017149@qq.com>
2020-05-20 15:33:59 +0800

    tijiao

--- js/index.js
+++ js/index.js
@@ -27,3 +27,16 @@ $('.nav-wrap').mouseleave(function() {
         });
     });
 });
+
+
+// part5切换
+let $contBarLi = $('.cont-bar li');
+let p5Cont = $('.cont-wrap .cont>div');
+$contBarLi.mouseenter(function() {
+    let index = $(this).index();
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+    p5Cont.eq(index).siblings().fadeOut();
+    p5Cont.eq(index).fadeIn();
+
+});
